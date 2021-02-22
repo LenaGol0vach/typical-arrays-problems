7e76e85e83a42fb59ed6865fbc41f8b98c95f3f2
LenaGol0vach <leshapantin@icloud.com>
2021-02-22 21:03:58 +0300

    feat: add JS code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
+exports.min = function min(...array) {
+    return Math.min(...array);
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(...array) {
+    return Math.max(...array);
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(...array) {
+    let sum = 0;
+    for (let item of array) {
+        sum += item;
+    }
+    return sum / array.length;
+};
